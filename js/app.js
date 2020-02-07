abf6add09450b2a392801478b3f831bf45413788
Romany Sefen <u1662160@unimail.hud.ac.uk>
2020-02-07 00:00:39 +0000

    dispaly weather of Selected city

--- js/app.js
+++ js/app.js
@@ -1,29 +1,19 @@
+import { initData } from "./functions/filterSearch.js";
 import { callApi } from "./functions/callApi.js";
 import { getCityWeather } from "./functions/getCityWeather.js";
 
-// const getCityWeather = async (city = "london") => {
-//   let cityToUpper = city[0].toUpperCase() + city.slice(1);
-//   cityTitle.innerHTML = cityToUpper + "'s  Weather  ";
-//   await callApi(city);
-// };
-
-// getCityWeather("cairo");
-
-/*generator function */
-
 function* getWeatherOf(city = "london") {
-  yield callApi(city);
-  yield getCityWeather(city);
+  const api = yield callApi(city);
+  const weather = yield getCityWeather(city);
 }
 
-const parisWeather = getWeatherOf("paris");
-parisWeather.next();
-parisWeather.next();
+initData();
 
-const londonWeather = getWeatherOf("london");
-londonWeather.next();
-londonWeather.next();
-
-const cairoWeather = getWeatherOf("cairo");
-cairoWeather.next();
-cairoWeather.next();
+const citiesSelector = [...document.querySelectorAll(".suggestion")];
+citiesSelector.map(city => {
+  return city.addEventListener("click", () => {
+    console.log(city.textContent);
+    getWeatherOf(city.textContent).next();
+  });
+});
+console.log(citiesSelector);
--- js/functions/filterSearch.js
+++ js/functions/filterSearch.js
@@ -1,19 +1,19 @@
 const searchInput = document.querySelector("#searchInput");
 
-let cities = ["cairo", "london", "leeds", "paris"];
+let cities = ["cairo", "london", "leeds", "paris", "huddersfield"];
 
 const getCitiesList = arrList => {
-  const resultsUL = document.querySelector("#results");
+  const resultsDiv = document.querySelector("#results");
   let p = "";
 
   arrList.map(city => {
     p += `<p class="alert alert-info suggestion "> ${city[0].toUpperCase() +
       city.slice(1)} </p> `;
   });
-  resultsUL.innerHTML = p;
+  resultsDiv.innerHTML = p;
 };
 
-getCitiesList(cities);
+// getCitiesList(cities);
 
 const filterResults = (filtered = [], term) => {
   const suggestions = filtered.filter(item => {
@@ -22,7 +22,12 @@ const filterResults = (filtered = [], term) => {
   return suggestions;
 };
 
-searchInput.addEventListener("keyup", e => {
-  const results = filterResults(cities, e.target.value);
-  getCitiesList(results);
-});
+export const initData = () => {
+  getCitiesList(cities);
+  searchInput.addEventListener("keyup", e => {
+    const results = filterResults(cities, e.target.value);
+    getCitiesList(results);
+
+    return results;
+  });
+};
